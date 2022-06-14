c054e04d5a5e634e61314bbb2ade3761854054bd
jamig7 <69559996+jamig7@users.noreply.github.com>
2022-06-14 22:07:35 +1000

    Update base.js

--- js/base.js
+++ js/base.js
@@ -5,7 +5,7 @@ eng.google = {
 	pageTitle: "SearXNG",
 	logo: "google.png",
 	places: {
-		'Web'    : ["https://gowogle.voring.me/search?q=%query%", false],
+		'Web'    : ["https://search.albony.xyz/search?q=%query%", false],
 		'Images' : ["https://searx.tiekoetter.com/search?=%query%&category_images=on", false],
 		'Maps'   : ["https://searx.tiekoetter.com/search?=%query%&category_map=on", false]
 	}
